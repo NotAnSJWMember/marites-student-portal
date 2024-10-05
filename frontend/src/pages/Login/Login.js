7d018191c176ba3d7a156ee16902d476747191ca
raneac <kaiserlaconfiture@gmail.com>
2024-10-05 22:42:23 +0800

    ugh

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { EyeOutline, LogoGoogle } from "react-ionicons";
-import logoIcon from "../../assets/images/logo.png";
 import styles from "./Login.module.scss";
+
+import logoIcon from "../../assets/images/logo.png";
+import { IoEyeOutline } from "@react-icons/all-files/io5/IoEyeOutline";
+import { IoLogoGoogle } from "@react-icons/all-files/io5/IoLogoGoogle";
 
 const Login = () => {
    return (
@@ -11,7 +13,7 @@ const Login = () => {
                <img src={logoIcon} alt="Marites University Seal" />
             </div>
             <div className={styles.head}>
-               <h1 className={styles.head}>Student Portal</h1>
+               <h1 className={styles.title}>Student Portal</h1>
                <p>Sign in to start your session</p>
             </div>
             <form>
@@ -21,7 +23,7 @@ const Login = () => {
                <div className={styles.inputMerge}>
                   <input type="password" autoComplete="false" />
                   <span className={styles.inputIcon}>
-                     <EyeOutline color="gray" height="20px" width="20px" />
+                     <IoEyeOutline color="gray" height="20px" width="20px" />
                   </span>
                </div>
                <a href="forgot.html" className={styles.ctaForgot}>
@@ -33,7 +35,7 @@ const Login = () => {
                   <div className={styles.line}></div>
                </div>
                <button className={styles.googleBtn}>
-                  <LogoGoogle color="white" />
+                  <IoLogoGoogle color="white" />
                   Sign in via Student Email
                </button>
             </form>
--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,10 +1,114 @@
 import React from "react";
 import styles from "./Register.module.scss";
+import { useForm } from "react-hook-form";
+
+import logoIcon from "../../assets/images/logo.png";
+import { IoEyeOutline } from "@react-icons/all-files/io5/IoEyeOutline";
 
 const Register = () => {
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm();
+
+   const onSubmit = (data) => {
+      console.log(data);
+   };
+
    return (
-      <div className={styles.formContainer}>
-         <div className={styles.container}></div>
+      <div className={styles.content}>
+         <div className={styles.container}>
+            <div className={styles.sealContainer}>
+               <img src={logoIcon} alt="Marites University Seal" />
+               <div className={styles.head}>
+                  <h1 className={styles.title}>Portal Registration</h1>
+                  <p>Create an account in Marites University</p>
+               </div>
+            </div>
+            <form
+               className={styles.formContainer}
+               onSubmit={handleSubmit(onSubmit)}
+            >
+               <div className={styles.nameColumn}>
+                  <div className={styles.firstName}>
+                     <label>
+                        First Name{" "}
+                        {errors.firstName && (
+                           <span className={styles.errorMsg}>
+                              ({errors.firstName.message})
+                           </span>
+                        )}
+                     </label>
+                     <input
+                        type="text"
+                        autoComplete="false"
+                        {...register("firstName", {
+                           required: "First name is required",
+                        })}
+                     />
+                  </div>
+                  <div className={styles.lastName}>
+                     <label>
+                        Last Name{" "}
+                        {errors.lastName && (
+                           <span className={styles.errorMsg}>
+                              ({errors.lastName.message})
+                           </span>
+                        )}
+                     </label>
+                     <input
+                        type="text"
+                        autoComplete="false"
+                        {...register("lastName", {
+                           required: "Last name is required",
+                        })}
+                     />
+                  </div>
+               </div>
+               <div>
+                  <label>
+                     Username{" "}
+                     {errors.username && (
+                        <span className={styles.errorMsg}>
+                           ({errors.username.message})
+                        </span>
+                     )}
+                  </label>
+                  <input
+                     type="text"
+                     autoComplete="false"
+                     {...register("username", {
+                        required: "Username is required",
+                     })}
+                  />
+               </div>
+               <div>
+                  <label>
+                     Password{" "}
+                     {errors.password && (
+                        <span className={styles.errorMsg}>
+                           ({errors.password.message})
+                        </span>
+                     )}
+                  </label>
+                  <div className={styles.inputMerge}>
+                     <input
+                        type="password"
+                        autoComplete="false"
+                        {...register("password", {
+                           required: "Password is required",
+                        })}
+                     />
+                     <span className={styles.inputIcon}>
+                        <IoEyeOutline color="gray" height="20px" width="20px" />
+                     </span>
+                  </div>
+               </div>
+
+               <button type="submit">Create account</button>
+            </form>
+         </div>
       </div>
    );
 };
