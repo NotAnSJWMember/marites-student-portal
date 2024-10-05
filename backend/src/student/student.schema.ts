fcc50c77dd708decc2eef6132c8bb6642e642a02
raneac <kaiserlaconfiture@gmail.com>
2024-10-05 20:13:36 +0800

    update crud in student

--- backend/src/student/student.schema.ts
+++ backend/src/student/student.schema.ts
@@ -2,3 +2,23 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type StudentDocument = Student & Document;
+
+@Schema()
+export class Student {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  course: string;
+
+  @Prop({ required: true })
+  year: number;
+
+  @Prop({ required: true, enum: ['Male', 'Female', 'Other'] })
+  sex: string;
+
+  @Prop({ required: true })
+  createdAt: Date;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
--- backend/src/student/student.service.ts
+++ backend/src/student/student.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Student, StudentDocument } from './student.schema';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectModel(Student.name) private studentModel: Model<StudentDocument>,
+  ) {}
+
+  async create(studentData: Partial<Student>): Promise<Student> {
+    const newStudent = new this.studentModel(studentData);
+    return newStudent.save();
+  }
+
+  async findAll(): Promise<Student[]> {
+    return this.studentModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Student> {
+    return this.studentModel.findById(id).exec();
+  }
+
+  async update(id: string, studentData: Partial<Student>): Promise<Student> {
+    return this.studentModel
+      .findByIdAndUpdate(id, studentData, { new: true })
+      .exec();
+  }
+
+  async delete(id: string): Promise<Student> {
+    return this.studentModel.findByIdAndDelete(id).exec();
+  }
+}
