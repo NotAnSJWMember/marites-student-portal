57230291e42fcf9fdcecd9f4825bd5a802c0fd8a
raneac <kaiserlaconfiture@gmail.com>
2024-10-05 20:27:58 +0800

    save

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentModule } from './students/student.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as dotenv from 'dotenv';
-
 async function bootstrap() {
   dotenv.config();
+  console.log('MongoDB URI:', process.env.MONGODB_URI);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
